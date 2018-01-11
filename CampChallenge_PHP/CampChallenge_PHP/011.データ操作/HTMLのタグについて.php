7fc327a700c16953db58f135f3ef7b7eaab9e5f5
guest1Day <guest1Day>
2018-01-11 16:41:33 +0900

    タグについて

---
+++ CampChallenge_PHP/CampChallenge_PHP/011.データ操作/HTMLのタグについて.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>てす</title>
+  </head>
+  <body bgcolor= "#ffffff" text = "#000000" link = "#0000ff">
+    文章
+  </body>
+</html>
